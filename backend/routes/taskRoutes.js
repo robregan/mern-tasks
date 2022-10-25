2254e13d33896b7d77ab44797a99f92a7747ce2c
rob <robert_regan86@yahoo.com>
2022-10-25 19:52:54 -0400

    delete and put/patch done

--- backend/routes/taskRoutes.js
+++ backend/routes/taskRoutes.js
@@ -1,10 +1,19 @@
 const express = require('express')
-const { createTask, getTasks } = require('../controllers/taskController')
+const {
+  createTask,
+  getTasks,
+  getTask,
+  deleteTask,
+  updateTask,
+} = require('../controllers/taskController')
 const app = express()
 const Task = require('../model/taskModel')
 const router = express.Router()
 
 router.post('/api/tasks', createTask)
 router.get('/api/tasks', getTasks)
+router.get('/api/tasks/:id', getTask)
+router.delete('/api/tasks/:id', deleteTask)
+router.patch('/api/tasks/:id', updateTask)
 
 module.exports = router
